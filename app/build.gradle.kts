4a2c94e902e05916090ba4cc160161fc3b2dd4ae
Jose Alfonso Mora <josealfonso@fpfomento.com>
2025-02-02 11:13:17 +0100

    Fix gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,13 @@
 import com.google.devtools.ksp.gradle.model.Ksp
-
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    id("com.google.devtools.ksp") //version "2.1.0-1.0.29" // Si es necesario usar la versión de KSP explícita
-   // id("androidx.room") //version "2.6.0" apply false
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
-
 android {
     namespace = "com.empresa.aplicacion"
-    compileSdk = 34
+    compileSdk = 35
     ksp {
         arg("room.schemaLocation", "$projectDir/schemas")
     }
@@ -36,26 +34,19 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
     }
-  //  composeOptions {
-    //    kotlinCompilerExtensionVersion = "1.5.1"
- //   }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -64,6 +55,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -71,24 +68,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
-    implementation(libs.androidx.room.runtime)   // Usamos el alias de Room de libs.versions.toml
-    implementation(libs.androidx.room.ktx)       // Usamos el alias de Room KTX
-    ksp(libs.androidx.room.compiler)             // Usamos el alias de Room Compiler para KSP
-    testImplementation(libs.androidx.room.testing) // Usamos el alias para las pruebas de Room
-
-    //Hilt
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
-
-    val room_version = "2.6.1"
-    ksp("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-runtime:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-    implementation("androidx.room:room-rxjava2:$room_version")
-    implementation("androidx.room:room-rxjava3:$room_version")
-    implementation("androidx.room:room-guava:$room_version")
-    testImplementation("androidx.room:room-testing:$room_version")
-    implementation("androidx.room:room-paging:$room_version")
 }
